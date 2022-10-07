cd7c961f2bca197987d55b8bd07d220dce9469c8
Азамат Джораев <azamat.jorayev@iogru.com>
2022-10-07 07:39:06 +0300

    initial commit

---
+++ next.config.js
@@ -0,0 +1,24 @@
+/** @type {import('next').NextConfig} */
+
+const withTM = require('next-transpile-modules')([
+  '@fullcalendar/common',
+  '@babel/preset-react',
+  '@fullcalendar/common',
+  '@fullcalendar/daygrid',
+  '@fullcalendar/interaction',
+  '@fullcalendar/react',
+  '@fullcalendar/timegrid',
+  '@fullcalendar/list',
+  '@fullcalendar/timeline'
+]);
+
+const nextConfig = withTM({
+  reactStrictMode: true,
+  images: {
+    loader: 'akamai',
+    path: '',
+  },
+  assetPrefix: './',
+});
+
+module.exports = nextConfig;
