d7ed047776bad536e4424616d18894102b23d46e
azamatos <87179356+azamatos@users.noreply.github.com>
2022-10-07 04:52:22 +0000

    Deploying to gh-pages from @ azamatos/berry-dashboard@4c21fec7ff8dc12aba5f23e3c66fc31b3b5d0371 🚀

---
+++ _next/static/chunks/pages/index-edae94876c78c932.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[5405],{45301:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/",function(){return u(74262)}])}},function(n){n.O(0,[939,4231,2175,5675,6066,7911,4262,9774,2888,179],(function(){return _=45301,n(n.s=_);var _}));var _=n.O();_N_E=_}]);
